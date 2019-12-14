8ec5d8324f1a05ea7d8ee63e260eb946b0e01c71
youtous <contact@youtous.me>
2019-12-14 00:16:06 -0500

    fix lone rangers

--- sudoku.cpp
+++ sudoku.cpp
@@ -711,45 +711,22 @@ int lonerangerStrategy(SudokuBoard &board) {
             // row
             auto const &positionRow = rowsCellsValues[i][value];
             if (positionRow.first >= 0 && positionRow.second >= 0) {
-                if (board.testValueInCellFromCompute(positionRow.first, positionRow.second, value)) {
-                    // lone ranger found, set value
-                    board.setValueAndUpdatePossibilities(positionRow.first, positionRow.second, value);
-                    solvedCells += 1;
-                } else {
-                    // todo : check this
-                    // value is not settable, maybe an other cell took its place, remove possible value from this cell
-                    board.removePossibleValueForCell(positionRow.first, positionRow.second, value);
-                    // todo fix this => a bug will occur due to solvedCell value update
-                    solvedCells += 1;
-                }
+                board.setValueAndUpdatePossibilities(positionRow.first, positionRow.second, value);
+                solvedCells += 1;
             }
 
             // col
             auto const &positionCol = columnsCellsValues[i][value];
             if (positionCol.first >= 0 && positionCol.second >= 0) {
-                if (board.testValueInCellFromCompute(positionCol.first, positionCol.second, value)) {
-                    // lone ranger found, set value
-                    board.setValueAndUpdatePossibilities(positionCol.first, positionCol.second, value);
-                    solvedCells += 1;
-                } else {
-                    // value is not settable, maybe an other cell took its place, remove possible value from this cell
-                    board.removePossibleValueForCell(positionCol.first, positionCol.second, value);
-                    solvedCells += 1;
-                }
+                board.setValueAndUpdatePossibilities(positionCol.first, positionCol.second, value);
+                solvedCells += 1;
             }
 
             // block
             auto const &positionBlock = blocksCellsValues[i][value];
             if (positionBlock.first >= 0 && positionBlock.second >= 0) {
-                if (board.testValueInCellFromCompute(positionBlock.first, positionBlock.second, value)) {
-                    // lone ranger found, set value
-                    board.setValueAndUpdatePossibilities(positionBlock.first, positionBlock.second, value);
-                    solvedCells += 1;
-                } else {
-                    // value is not settable, maybe an other cell took its place, remove possible value from this cell
-                    board.removePossibleValueForCell(positionBlock.first, positionBlock.second, value);
-                    solvedCells += 1;
-                }
+                board.setValueAndUpdatePossibilities(positionBlock.first, positionBlock.second, value);
+                solvedCells += 1;
             }
         }
     }
